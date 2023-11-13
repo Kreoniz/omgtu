00e68fe18e7918c45f8759e9f8c6236aa67c54ff
Kreoniz <kreoniz@mail.ru>
2023-11-13 12:20:46 +0600

    Add a solution and change folder hierarchy

---
+++ algorithms/november/13.11.23/1.cs
@@ -0,0 +1,31 @@
+using System;
+
+// На вход подается строка, определить наибольшую длину подстроки-палиндрома
+class Negative {
+    public static bool isPalindrome(string text) {
+        for (int i = 0; i < text.Length; i++) {
+            if (text[i] != text[text.Length - i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void Main(string[] args) {
+        string text = Console.ReadLine();
+        int max_length = 0;
+
+        for (int i = 0; i < text.Length; i++) {
+            for (int j = i + 1; j < text.Length; j++) {
+                if (isPalindrome(text.Substring(i, j - i - 1))) {
+                    if (max_length < j - i) {
+                        max_length = j - i;
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"Длина наибольшей подстроки-палиндрома: {max_length}");
+    }
+}
